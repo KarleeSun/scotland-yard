929e9e3c455c90942bf631dc6087044b93db1654
blackbluetea <93391620+blackbluetea@users.noreply.github.com>
2022-03-30 11:55:54 +0100

    first commit

---
+++ src/main/java/uk/ac/bris/cs/scotlandyard/ui/ai/MyAi.java
@@ -0,0 +1,37 @@
+package uk.ac.bris.cs.scotlandyard.ui.ai;
+
+import java.util.HashSet;
+import java.util.Random;
+import java.util.Set;
+import java.util.concurrent.TimeUnit;
+import java.util.concurrent.atomic.AtomicBoolean;
+
+import javax.annotation.Nonnull;
+
+
+import io.atlassian.fugue.Pair;
+import uk.ac.bris.cs.scotlandyard.model.*;
+
+public class MyAi implements Ai {
+
+	@Nonnull @Override public String name() { return "Xbot"; }
+
+	@Nonnull @Override public Move pickMove(
+			@Nonnull Board board,
+			Pair<Long, TimeUnit> timeoutPair) {
+		// returns a random move, replace with your own implementation
+//		var moves = board.getAvailableMoves().asList();
+//		return moves.get(new Random().nextInt(moves.size()));
+
+
+
+
+        return null;
+	}
+
+	//判断这个位置detective能不能一步走到
+	public Move oneMoveNotReachMrX(@Nonnull Board board){
+        var moves = board.getAvailableMoves().asList();
+    }
+
+}
