7c413e18a03635d4c38ad71790a679e4c152d859
blackbluetea <93391620+blackbluetea@users.noreply.github.com>
2022-03-30 12:15:48 +0100

    initial

--- src/main/java/uk/ac/bris/cs/scotlandyard/ui/ai/MyAi.java
+++ src/main/java/uk/ac/bris/cs/scotlandyard/ui/ai/MyAi.java
@@ -19,6 +19,7 @@ public class MyAi implements Ai {
 	@Nonnull @Override public Move pickMove(
 			@Nonnull Board board,
 			Pair<Long, TimeUnit> timeoutPair) {
+        //1
 		// returns a random move, replace with your own implementation
 //		var moves = board.getAvailableMoves().asList();
 //		return moves.get(new Random().nextInt(moves.size()));
