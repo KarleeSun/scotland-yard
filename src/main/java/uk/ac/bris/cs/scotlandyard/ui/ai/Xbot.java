2851e2e84f5e1d0f996a0f505e3156b4910fac04
Karlee <1907889052@qq.com>
2022-04-06 16:08:08 +0100

    dijkstra

--- src/main/java/uk/ac/bris/cs/scotlandyard/ui/ai/Xbot.java
+++ src/main/java/uk/ac/bris/cs/scotlandyard/ui/ai/Xbot.java
@@ -1,8 +1,6 @@
 package uk.ac.bris.cs.scotlandyard.ui.ai;
 
-import java.util.HashSet;
-import java.util.Random;
-import java.util.Set;
+import java.util.*;
 import java.util.concurrent.TimeUnit;
 import java.util.concurrent.atomic.AtomicBoolean;
 
@@ -12,26 +10,34 @@ import javax.annotation.Nonnull;
 import io.atlassian.fugue.Pair;
 import uk.ac.bris.cs.scotlandyard.model.*;
 
-public class MyAi implements Ai {
+public class Xbot implements Ai {
+
 
 	@Nonnull @Override public String name() { return "Xbot"; }
 
 	@Nonnull @Override public Move pickMove(
 			@Nonnull Board board,
 			Pair<Long, TimeUnit> timeoutPair) {
-		// returns a random move, replace with your own implementation
-//		var moves = board.getAvailableMoves().asList();
-//		return moves.get(new Random().nextInt(moves.size()));
+		List<Integer> l = new ArrayList<Integer>();
+		l.add(84);
+		l.add(26);
 
-
-
-
-        return null;
+		DijkstraMinHeap mp = new DijkstraMinHeap(1, l , board);
+		mp.getDetectivesDistance();
+		var moves = board.getAvailableMoves().asList();
+		return moves.get(new Random().nextInt(moves.size()));
 	}
 
+
+
+
+
+
+
 	//判断这个位置detective能不能一步走到
-	public Move oneMoveNotReachMrX(@Nonnull Board board){
-        var moves = board.getAvailableMoves().asList();
-    }
+//	public Move oneMoveNotReachMrX(@Nonnull Board board){
+//        var moves = board.getAvailableMoves().asList();
+//		return null;
+//    }
 
 }
