6570341dc74c9ba0cb0086e6d882f9db948a06f2
blackbluetea <93391620+blackbluetea@users.noreply.github.com>
2022-04-06 15:51:33 +0100

    shit

--- src/main/java/uk/ac/bris/cs/scotlandyard/ui/ai/MyAi.java
+++ src/main/java/uk/ac/bris/cs/scotlandyard/ui/ai/MyAi.java
@@ -1,37 +1,106 @@
 package uk.ac.bris.cs.scotlandyard.ui.ai;
 
-import java.util.HashSet;
-import java.util.Random;
-import java.util.Set;
+import java.util.*;
 import java.util.concurrent.TimeUnit;
 import java.util.concurrent.atomic.AtomicBoolean;
 
 import javax.annotation.Nonnull;
 
-
+import com.google.common.collect.ImmutableList;
 import io.atlassian.fugue.Pair;
+
 import uk.ac.bris.cs.scotlandyard.model.*;
+
 
 public class MyAi implements Ai {
 
-	@Nonnull @Override public String name() { return "Xbot"; }
+    @Nonnull
+    @Override
+    public String name() {
+        return "Xbot";
+    }
 
-	@Nonnull @Override public Move pickMove(
-			@Nonnull Board board,
-			Pair<Long, TimeUnit> timeoutPair) {
-		// returns a random move, replace with your own implementation
-//		var moves = board.getAvailableMoves().asList();
+
+    @Nonnull
+    @Override
+    public Move pickMove(
+            @Nonnull Board board,
+            Pair<Long, TimeUnit> timeoutPair) {
+
+        // returns a random move, replace with your own implementation
+        var moves = getMoves(board);
+        distanceMark(board);
 //		return moves.get(new Random().nextInt(moves.size()));
 
 
-
-
         return null;
-	}
+    }
 
-	//判断这个位置detective能不能一步走到
-	public Move oneMoveNotReachMrX(@Nonnull Board board){
-        var moves = board.getAvailableMoves().asList();
+    //get all available moves and return as a list
+    private static ImmutableList<Move> getMoves(@Nonnull Board board) {
+        return board.getAvailableMoves().asList();
+    }
+
+
+    //decide which move will be farthest from the nearest detective(s)
+    //and cast the distance into a mark
+    private static int distanceMark(@Nonnull Board board) {
+
+        /* -------------------------------------------------------------------
+            when is mrX's turn, get mrX's location from moves
+            get detective locations from board
+         */
+
+        //get mrX location
+        int mrXLoc = board.getAvailableMoves().stream().iterator().next().source();
+
+        System.out.println("mrX loc: "+mrXLoc);
+
+        //get pieces of detectives
+        List<Piece> allDetectivePieces = new ArrayList<Piece>();
+        allDetectivePieces.addAll(board.getPlayers());
+        allDetectivePieces.remove("MRX");
+
+        List<Piece.Detective> detectives = new ArrayList<>();
+        for(Piece detective : allDetectivePieces){
+            if(detective.isDetective()) detectives.add((Piece.Detective)detective);
+        }
+
+        //要删掉的东西
+        System.out.println("pieces: " + allDetectivePieces);
+        System.out.println("detectives: "+detectives);
+
+        //get detectives locations and store them in a list
+        List<Integer> detectiveLocations = new ArrayList<>();
+
+        for (Piece.Detective detectivePiece : detectives) {
+            detectiveLocations.add(board.getDetectiveLocation(detectivePiece).get());
+        }
+
+        System.out.println("detective locs: "+detectiveLocations);
+
+
+        /* ----------------------------------------------------------
+            if it's detectives' turn, get mrX location from log
+            get detective locations from moves
+         */
+
+
+        return 0;
+    }
+
+    /*
+    available moves和detectives的距离
+    这个点有几种交通工具 都是哪些
+    detectives还剩什么票
+    和这个点相连的点有几个
+    reveal
+     */
+    private static int giveMark(@Nonnull Board board){
+
+        return 0;
     }
 
 }
+
+
