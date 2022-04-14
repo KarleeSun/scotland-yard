f96462d4dc11b5f56bf9aa0ad0137a9402ac8797
Karlee <1907889052@qq.com>
2022-04-14 14:41:38 +0100

    treeNode

--- src/main/java/uk/ac/bris/cs/scotlandyard/ui/ai/TreeNode.java
+++ src/main/java/uk/ac/bris/cs/scotlandyard/ui/ai/TreeNode.java
@@ -12,5 +12,35 @@ public class TreeNode {
     private TreeNode parent;
     private List<TreeNode> children;
 
-    
+    public TreeNode(Move move, int score){
+        this.move = move;
+        this.score = score;
+    }
+
+    public void addChild(TreeNode child){
+        child.setParent(this);
+        this.children.add(child);
+    }
+
+    public void addChild(Move move, int score){
+        TreeNode child = new TreeNode(move, score);
+        this.children.add(child);
+    }
+
+    public void setParent(TreeNode parent) {this.parent = parent;}
+
+    public int getScore() {return score;}
+    public Move getMove() {return move;}
+
+    public TreeNode getParent() {return parent;}
+
+    public List<TreeNode> getChildren() {return children;}
+
+    public void setAlpha(int alpha) {this.alpha = alpha;}
+
+    public int getAlpha() {return alpha;}
+
+    public void setBeta(int beta) {this.beta = beta;}
+
+    public int getBeta() {return beta;}
 }
