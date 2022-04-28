0f09d98676a7935176c298af346a979907b7e16e
blackbluetea <93391620+blackbluetea@users.noreply.github.com>
2022-04-29 00:48:35 +0100

    comments done

--- src/test/java/TestForMiniMax.java
+++ src/test/java/TestForMiniMax.java
@@ -14,15 +14,23 @@ import static uk.ac.bris.cs.scotlandyard.model.Piece.MrX.MRX;
 import static uk.ac.bris.cs.scotlandyard.model.ScotlandYard.*;
 import static uk.ac.bris.cs.scotlandyard.model.ScotlandYard.defaultDetectiveTickets;
 
+/**
+ * test for minimax algorithm to find whether we have done it right
+ * and see the time in millisecond to help us improve the performance
+ */
+
 public class TestForMiniMax {
     public TestForMiniMax() {
         setup();
     }
 
+    //load the default Scotlandyard map
     private ImmutableValueGraph<Integer, ImmutableSet<ScotlandYard.Transport>> defaultGraph;
 
+    //unit test for minimax
     @Test
     public void testForMiniMax() {
+        //create players
         var mrX = new Player(MRX, defaultMrXTickets(), 11);
         var red = new Player(RED, defaultDetectiveTickets(), 91);
         var green = new Player(GREEN, defaultDetectiveTickets(), 29);
@@ -31,6 +39,7 @@ public class TestForMiniMax {
         var yellow = new Player(YELLOW, defaultDetectiveTickets(), 138);
         Board.GameState state = new MyGameStateFactory().build(standard24MoveSetup(),
                 mrX, red, green, blue, white, yellow);
+        //load minimax and test
         Minimax minimax = new Minimax();
         minimax.test = true;
         long start = System.currentTimeMillis();
@@ -44,25 +53,27 @@ public class TestForMiniMax {
         System.out.println("avg distance use:" + minimax.timeList.stream().mapToLong(t -> t).average().orElseThrow() + "ms");
     }
 
+    //record node number
     public void countNode(Minimax.TreeNode node, Map<Integer, List<Minimax.TreeNode>> buffer, int depth) {
         List<Minimax.TreeNode> children = node.getChildren();
         buffer.putIfAbsent(depth, new ArrayList<>());
         buffer.get(depth).addAll(children);
-        children.forEach(child -> {
-            countNode(child, buffer, depth + 1);
-        });
+        children.forEach(child -> countNode(child, buffer, depth + 1));
     }
 
+    //programme setup
     public void setup() {
         try {
             defaultGraph = readGraph(Resources.toString(Resources.getResource(
                             "graph.txt"),
                     StandardCharsets.UTF_8));
         } catch (IOException e) {
+            //throw exception if can't load the graph
             throw new RuntimeException("Unable to read game graph", e);
         }
     }
 
+    //set up a standard 24-round game
     public GameSetup standard24MoveSetup() {
         return new GameSetup(defaultGraph, STANDARD24MOVES);
     }
